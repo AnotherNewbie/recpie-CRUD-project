a74b4571e5d9f9b83221bd5a1077080f50b4be53
Michael Holthausen <mgholthausen@gmail.com>
2021-12-17 13:33:37 -0500

    Updates

---
+++ precache-manifest.d6a8dac31353b2e64ea8ebabbc3e058d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8347ef8b15da88fddf682a9efb9e9ba9",
+    "url": "/recpie-CRUD-project/index.html"
+  },
+  {
+    "revision": "f88d125e6d900b4024b2",
+    "url": "/recpie-CRUD-project/static/css/main.a59acca2.chunk.css"
+  },
+  {
+    "revision": "db02650134225a655ed4",
+    "url": "/recpie-CRUD-project/static/js/2.32cef4b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recpie-CRUD-project/static/js/2.32cef4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88d125e6d900b4024b2",
+    "url": "/recpie-CRUD-project/static/js/main.ccfd5a7b.chunk.js"
+  },
+  {
+    "revision": "fcfafc6570f058502605",
+    "url": "/recpie-CRUD-project/static/js/runtime-main.64a29edd.js"
+  }
+]);
