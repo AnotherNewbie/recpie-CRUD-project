e4dbce3be70009f91fdc7af41f45f16a9b558296
Michael Holthausen <mgholthausen@gmail.com>
2021-12-17 11:02:47 -0500

    worked on recipeList form

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,10 +1,30 @@
 import React from "react";
 
-function RecipeList() {
+function RecipeList({recipes}) {
   
   // TODO: Display the list of recipes using the structure of table that is provided.
   // TODO: Create at least one additional component that is used by this component.
   // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+  if(recipes.length){
+    const recipeList = recipes.map((recipe) => {
+      <tr>
+        <td className="content_td">
+          <p>{recipe.name}</p>
+        </td>
+        <td className="content_td">
+          <p>{recipe.cuisine}</p>
+        </td>
+        <td className="content_td">
+          <img className="Photo" src={recipe.photo}></img>
+        </td>
+        <td className="content_td">
+          <p>{recipe.ingredients}</p>
+        </td>
+        <td className="content_td">
+          <p>{recipe.preparation}</p>
+        </td>
+      </tr>
+    })
 
   return (
     <div className="recipe-list">
@@ -16,11 +36,13 @@ function RecipeList() {
           </tr>
         </thead>
         <tbody>
-        
+          {recipeList}        
         </tbody>
       </table>
     </div>
   );
+  } else {
+    return null;
+  }
 }
-
 export default RecipeList;
